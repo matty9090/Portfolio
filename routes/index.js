e757cd880feca02fda47c93af595fbdb78b9d9fe
matty9090 <lowe.matthew@mail.com>
2019-09-08 12:22:45 +0100

    Add projects page content

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var projects = require('../app/projects')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     var proj = new projects();
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var projects = require('../app/projects')
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('projects', { title: 'Matthew Lowe | Game Developer', 'page': 'projects' });
+    var proj = new projects();
+
+    res.render('projects', {
+        title: 'Matthew Lowe | Game Developer',
+        page: 'projects',
+        projects: proj.projects
+    });
 });
 
 module.exports = router;
